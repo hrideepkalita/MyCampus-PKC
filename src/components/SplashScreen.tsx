637418cb512ff71766cb91366133e7e8fc4e42c2
Himangshu kalita <rangiavlog@gmail.com>
2026-06-18 21:11:51 +0530

    Update college name in SplashScreen component

--- src/components/SplashScreen.tsx
+++ src/components/SplashScreen.tsx
@@ -18,7 +18,7 @@ const SplashScreen = () => (
         MyCampus
       </h1>
       <p className="mt-3 text-sm text-white/60 text-center leading-relaxed">
-       <br/>Pub Kamrup College
+       <br/>Pub Kamrup College, Baihata Chariali
       </p>
     </motion.div>
     <div className="flex flex-1 flex-col items-center justify-end">
